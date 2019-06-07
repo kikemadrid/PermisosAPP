f1de1c5b05efb50faaec2d3ac23a72f7d01823e1
enrique <enrique>
2019-06-07 09:29:14 +0200

    Subida de proyecto a git

---
+++ app/src/main/java/dao/TiposViewHolder.java
@@ -0,0 +1,13 @@
+package dao;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class TiposViewHolder extends RecyclerView.ViewHolder {
+
+
+    public TiposViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
